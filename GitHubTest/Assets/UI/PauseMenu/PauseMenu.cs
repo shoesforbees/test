09e6b0c16a8c134c0d6d27faff1c1f0667a80b56
shoesforbees <60063554+shoesforbees@users.noreply.github.com>
2020-05-31 01:22:33 +0200

    Added Main Menu and Pause/resume

---
+++ GitHubTest/Assets/UI/PauseMenu/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(GameIsPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f; // Unfreeze the game
+        GameIsPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f; // Freeze the game
+        GameIsPaused = true;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1.0f; // Unfreeze the game
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
